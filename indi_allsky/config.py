a6f729987a520f6fed034bd56ed0da2d4ecfd729
ikeysolomon <134189416+ikeysolomon@users.noreply.github.com>
2026-09-02 20:50:51 +1000

    revert opacity

--- indi_allsky/config.py
+++ indi_allsky/config.py
@@ -230,7 +230,6 @@ class IndiAllSkyConfigBase(object):
             "MILKYWAY_GAMMA"    : 2.2,
             "MILKYWAY_BAND_WIDTH": 10.0,
             "MILKYWAY_FEATHER"  : 60.0,
-            "MILKYWAY_OPACITY"  : 0.35,
             "MILKYWAY_SATURATION": 1.4,
         },
         "KEOGRAM_ANGLE"         : 0.0,
